08b00e83f6460fe0e75e0959a6f316e11563aa45
Alam Guardin <alamguardin@gmail.com>
2023-09-13 21:56:25 -0600

    style: create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-	return <h1>Hello World</h1>
+	return (
+		<>
+			<Navbar></Navbar>
+		</>
+	)
 }
 
 export default App

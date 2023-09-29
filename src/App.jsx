bfdc115c822d7ee08d1e9729aebc62abd1a6949e
Alam Guardin <alamguardin@gmail.com>
2023-09-29 07:54:28 -0500

    Reset project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
 
 function App() {
 	return (
 		<>
-			<Navbar></Navbar>
-			<Hero></Hero>
+			<h1>Hello World</h1>
 		</>
 	)
 }

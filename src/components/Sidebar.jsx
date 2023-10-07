333edddb02a8f2e4ac768aaa32bf23fa57a785e8
Alam Guardin <alamguardin@gmail.com>
2023-10-06 22:47:29 -0500

    refactor: apply changes in fuction handle changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,16 +2,7 @@ function Sidebar({ properties, addProperties} ) {
 
     const handleChange = (e) => {
         let newProperties = {...properties}
-
-        if (e.target.id === 'inputColor') {
-            newProperties.background = e.target.value
-        } else if (e.target.id === 'inputBlur') {
-            newProperties.blur = e.target.value
-        } else if (e.target.id === 'inputOpacity') {
-            newProperties.opacity = e.target.value
-        } else if (e.target.id === 'inputSaturation') {
-            newProperties.saturation = e.target.value
-        }
+        newProperties[e.target.id] = e.target.value
 
         addProperties(newProperties)
     }
@@ -25,28 +16,28 @@ function Sidebar({ properties, addProperties} ) {
                 <div className="sidebar__tool">
                     <label htmlFor="" className="sidebar__label">Background</label>
                     <div className="sidebar__group">
-                        <input type="color" id="inputColor" className="sidebar__color" onChange={handleChange}/>
+                        <input type="color" id="background" className="sidebar__color" onChange={handleChange}/>
                         <input type="text" className="sidebar__input-color" disabled value={properties.background}/>
                     </div>
                 </div>
                 <div className="sidebar__tool">
                     <label htmlFor="" className="sidebar__label">Blur</label>
                     <div className="sidebar__group">
-                        <input type="range" id="inputBlur" className="sidebar__slider" min={0} max={25} onChange={handleChange}/>
+                        <input type="range" id="blur" className="sidebar__slider" min={0} max={25} onChange={handleChange}/>
                         <input type="text" className="sidebar__input" value={`${properties.blur}px`} disabled/>
                     </div>
                 </div>
                 <div className="sidebar__tool">
                     <label htmlFor="" className="sidebar__label">Opacity</label>
                     <div className="sidebar__group">
-                        <input type="range" id="inputOpacity" className="sidebar__slider" min={0} max={100} onChange={handleChange}/>
+                        <input type="range" id="opacity" className="sidebar__slider" min={0} max={100} onChange={handleChange}/>
                         <input type="text" className="sidebar__input" value={`${properties.opacity}%`} disabled/>
                     </div>
                 </div>
                 <div className="sidebar__tool">
                     <label htmlFor="" className="sidebar__label">Saturation</label>
                     <div className="sidebar__group">
-                        <input type="range" id="inputSaturation" className="sidebar__slider" min={0} max={200} onChange={handleChange}/>
+                        <input type="range" id="saturation" className="sidebar__slider" min={0} max={200} onChange={handleChange}/>
                         <input type="text" className="sidebar__input" value={`${properties.saturation}%`} disabled/>
                     </div>
                 </div>
